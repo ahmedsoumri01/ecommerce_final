9849ddde333bc2ea28a11355264f1442ff3da9ce
ahmed soumri <ahmedsoumri01@gmail.com>
2025-07-01 01:14:10 +0100

    fix midlle ware in categorie routes

--- server/routes/categories.routes.js
+++ server/routes/categories.routes.js
@@ -3,7 +3,10 @@ const express = require("express");
 const router = express.Router();
 
 // Import middlewares
-const { adminMiddleware } = require("../middlewares/auth.middleware");
+const {
+  adminMiddleware,
+  authMiddleware,
+} = require("../middlewares/auth.middleware");
 const { uploadSingle } = require("../middlewares/upload.middleware");
 
 // Import controller
@@ -16,6 +19,7 @@ router.get("/:id", categoriesController.getCategoryById);
 // Admin Protected Routes
 router.post(
   "/create",
+  authMiddleware,
   adminMiddleware,
   uploadSingle("image"),
   categoriesController.createCategory
@@ -23,11 +27,17 @@ router.post(
 
 router.put(
   "/:id",
+  authMiddleware,
   adminMiddleware,
   uploadSingle("image"),
   categoriesController.updateCategory
 );
 
-router.delete("/:id", adminMiddleware, categoriesController.deleteCategory);
+router.delete(
+  "/:id",
+  authMiddleware,
+  adminMiddleware,
+  categoriesController.deleteCategory
+);
 
 module.exports = router;
