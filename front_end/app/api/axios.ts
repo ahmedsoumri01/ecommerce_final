a5cb0555bdc108a31ba2a67182e4f105704f881e
ahmed soumri <ahmedsoumri01@gmail.com>
2025-07-03 11:23:35 +0100

    fix final

--- front_end/app/api/axios.ts
+++ front_end/app/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_ASSETS_URL,
+  baseURL: process.env.NEXT_PUBLIC_APP_URL,
   headers: {
     "Content-Type": "application/json",
   },
